367f0108aedc72dd40f8220df92286155d3d2219
being-devahmad <devahmad41@gmail.com>
2024-05-01 15:46:15 +0500

    meals commit

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,26 @@
+import classes from "./page.module.css";
+import Link from "next/link";
+
 export default function MealsPage(){
     return(
         <>
-            <h1>This is meals page</h1>
+            <header className={classes.header}>
+                <h1>
+                    Delicious Meals, created{" "}
+                    <span className={classes.highlight}>
+                        by you
+                    </span>
+                </h1>
+                <p>Choose your favourite recipe and cook yourself.</p>
+                <p className={classes.cta}>
+                    <Link href={'/meals/share'}>Share your favourite recipe</Link>
+                </p>
+
+            </header>
+
+            <main>
+
+            </main>
         </>
     )
 }
