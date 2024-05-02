a3575f6fe494ccabea7b3a1d1cc67c06b004a429
being-devahmad <devahmad41@gmail.com>
2024-05-03 00:28:33 +0500

    share meals commit

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,11 @@
 import classes from './page.module.css';
 import ImagePicker from "@/components/meals/image-picker";
+import shareMeal from "@/lib/actions";
 
 export default function ShareMealPage() {
+
+
+
     return (
         <>
             <header className={classes.header}>
@@ -11,7 +15,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form}>
+                <form className={classes.form} action={shareMeal}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -39,7 +43,7 @@ export default function ShareMealPage() {
                             required
                         ></textarea>
                     </p>
-                    <ImagePicker/>
+                    <ImagePicker name={"image"} label={"your image"}/>
                     <p className={classes.actions}>
                         <button type="submit">Share Meal</button>
                     </p>
